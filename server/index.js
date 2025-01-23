2da2968a5a7ab7033689113e288be9394e7d6a66
Amirul Faqih <fakiiahmad001@gmail.com>
2025-01-23 23:09:05 +0300

    Add comment to clarify the use of postRoutes for handling /posts routes in index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+// Use the postRoutes for all routes that start with /posts
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
